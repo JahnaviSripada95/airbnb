17c261c1657c1ceb5d6c748348eea8943ee5adbc
JahnaviSripada95 <rajasreesripada@gmail.com>
2024-06-29 21:06:49 +0530

    First commit

---
+++ models/listing.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    description:String,
+    image:{
+        
+        type: String,
+        default : "https://unsplash.com/photos/a-lone-tree-in-the-middle-of-a-foggy-field-sMsxUARkCos" ,
+        set: (v) => v===" "? "https://unsplash.com/photos/a-lone-tree-in-the-middle-of-a-foggy-field-sMsxUARkCos" : v,
+    },
+    price:Number,
+    location:String,
+    country:String,
+
+});
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
